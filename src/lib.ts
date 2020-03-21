9ce27a3bd8c8d05caa0d27409187e45afa57d166
hirantha <sahanhirantha@gmail.com>
2020-03-21 22:14:08 +0530

    util function on lib

--- src/lib.ts
+++ src/lib.ts
@@ -12,3 +12,24 @@ export function getClipboardText() {
 export function handleError(error: Error) {
     window.showErrorMessage(error.message);
 }
+
+export function pasteToMarker(content: string) {
+    const { activeTextEditor } = window;
+
+    return activeTextEditor?.edit(editBuilder => {
+        editBuilder.replace(activeTextEditor.selection, content);
+    });
+}
+
+export function getSelectedText(): Promise<string> {
+    const { selection, document } = window.activeTextEditor!;
+    return Promise.resolve(document.getText(selection).trim());
+}
+
+export const validateLength = (text: any) => {
+    if (text.length === 0) {
+        return Promise.reject(new Error("Nothing selected"));
+    } else {
+        return Promise.resolve(text);
+    }
+};
