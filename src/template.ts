34392b76c6264da469a95c870b538c5c93359b10
hirantha <sahanhirantha@gmail.com>
2020-03-22 01:56:35 +0530

    template work fine for simpe json and as one file

--- src/template.ts
+++ src/template.ts
@@ -1,3 +1,35 @@
-export function getClassTemplate(className:string,obj:any){
-    
+import {
+    getTypeofProperty
+} from "./lib";
+import * as changeCase from "change-case";
+
+export function getClassTemplate(className: string, obj: any): string {
+
+    return `${
+        Object.keys(obj)
+            .filter(key => getTypeofProperty(obj[key]) === "object")
+            .map(key => `import './${changeCase.snakeCase(key.toLowerCase())}.dart;`)
+            .reduce((a, b) => `${a}\n${b}`, "")
+        }
+
+    class ${className}{
+        ${
+        Object.keys(obj)
+            .map(key => `${mapTsTypeToDartType(getTypeofProperty(obj[key]), key, obj[key])} ${changeCase.camelCase(key.toLowerCase())};`)
+            .reduce((a, b) => `${a}\n${b}`)
+        }
+    }
+    `;
+}
+
+function mapTsTypeToDartType(type: string, key: String, obj: any): string {
+    const types: { [name: string]: string } = {
+        "integer": "int",
+        "string": "String",
+        "object": changeCase.pascalCase(key.toLowerCase()),
+        "map": `Map<String,String>`,
+        "array": `List<String>`,
+        "double": "double"
+    };
+    return types[type];
 }
